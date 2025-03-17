5d73170052c1fb37fb0135a9aede02aa4d0e3931
Abhinav <abhinavhooda223@gmail.com>
2025-03-17 23:45:26 +0530

    project-3 file setup

--- project-2-tip-calculator/script.js
+++ project-2-tip-calculator/script.js
@@ -1,5 +1,5 @@
 // access the input fields
 let bill = document.getElementById("bill").value;
 let percentage = document.getElementById("percentage").value;
-
+let btn = document.getElementById("btn");
 console.log(percentage,bill);
